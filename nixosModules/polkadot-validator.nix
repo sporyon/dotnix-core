dd4ca24493cffda48ff9c7d3b31766daa984e77c
tv <tv@krebsco.de>
2024-05-13 18:28:58 +0200

    polkadot-validator service: add upstream config
    Import systemd configuration from polkadot-sdk[1].
    
    [1]: https://github.com/paritytech/polkadot-sdk/blob/c106dbd095cddb3254ab85eca99bdaa997db2ca3/polkadot/scripts/packaging/polkadot.service

--- nixosModules/polkadot-validator.nix
+++ nixosModules/polkadot-validator.nix
@@ -7,10 +7,46 @@
       wantedBy = [
         "multi-user.target"
       ];
+      after = [
+        "network.target"
+      ];
       serviceConfig = {
         ExecStartPre = "${pkgs.polkadot}/bin/polkadot --version";
         ExecStart = "${pkgs.coreutils}/bin/sleep infinity";
         DynamicUser = true;
+        User = "polkadot";
+        Group = "polkadot";
+        Restart = "always";
+        RestartSec = 120;
+        CapabilityBoundingSet = "";
+        LockPersonality = true;
+        NoNewPrivileges = true;
+        PrivateDevices = true;
+        PrivateMounts = true;
+        PrivateTmp = true;
+        PrivateUsers = true;
+        ProtectClock = true;
+        ProtectControlGroups = true;
+        ProtectHostname = true;
+        ProtectKernelModules = true;
+        ProtectKernelTunables = true;
+        ProtectSystem = "strict";
+        RemoveIPC = true;
+        RestrictAddressFamilies = "AF_INET AF_INET6 AF_NETLINK AF_UNIX";
+        RestrictNamespaces = "false";
+        RestrictSUIDSGID = true;
+        SystemCallArchitectures = "native";
+        SystemCallFilter = [
+          "@system-service"
+          "landlock_add_rule landlock_create_ruleset landlock_restrict_self seccomp mount umount2"
+          "~@clock @module @reboot @swap @privileged"
+          "pivot_root"
+        ];
+        UMask = "0027";
+      };
+      unitConfig = {
+        Description = "Polkadot Node";
+        Documentation = "https://github.com/paritytech/polkadot";
       };
     };
   };
