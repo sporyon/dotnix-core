9c7cb4f81ee64c879bf91ac87b03a98bb270fe93
tv <tv@krebsco.de>
2024-05-13 16:36:04 +0200

    flake: add polkadot input and expose package

---
+++ flake.nix
@@ -0,0 +1,16 @@
+{
+  inputs = {
+    nixpkgs.url = "git+file:/var/src/nixpkgs-unstable";
+
+    polkadot.url = "github:andresilva/polkadot.nix";
+    polkadot.inputs.nixpkgs.follows = "nixpkgs";
+  };
+  outputs = inputs: {
+    # usage: nix build --no-link --print-out-paths .#polkadot
+    # test: $(nix build --no-link --print-out-paths .#polkadot)/bin/polkadot --version | grep -q ^polkadot
+    #   https://wiki.polkadot.network/docs/maintain-guides-how-to-validate-polkadot
+    #   says that the versions of all three executables[1] must be the same.
+    #   [1]: polkadot, polkadot-execute-worker, polkadot-prepare-worker
+    packages.x86_64-linux.polkadot = inputs.polkadot.packages.x86_64-linux.polkadot;
+  };
+}
