b8c83cbc9d2bb9399a0887fec2d92170131f9e9e
ra33it0 <sebastian@sporyon.io>
2024-06-02 16:31:28 +0200

    docker: configure polkadot validator

--- docker.nix
+++ docker.nix
@@ -6,6 +6,24 @@
     ({
       imports = [
         (inputs.nixpkgs + "/nixos/modules/virtualisation/docker-image.nix")
+        nixosModules.polkadot-validator
+      ];
+
+      users.users.dotnix = {
+        isNormalUser = true;
+        password = "dotnix";
+        extraGroups = [ "wheel" ];
+      };
+
+      # Enabling flakes in docker
+      nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+      # Validator configuration
+      dotnix.polkadot-validator.enable = true;
+      dotnix.polkadot-validator.name = "dotnix-docker";
+      dotnix.polkadot-validator.chain = "westend";
+      dotnix.polkadot-validator.extraArgs = [
+        "--db-storage-threshold=0"
       ];
     })
   ];
