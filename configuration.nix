b0339b7cec8e0d7c3186eebbf3f9d4cdc697f10a
ra33it0 <sebastian@sporyon.io>
2025-03-24 14:52:27 +0100

    selinux: add basic structure

---
+++ configuration.nix
@@ -0,0 +1,38 @@
+{ config, lib, pkgs, ... }: {
+  imports = [
+    ./selinux.nix
+  ];
+    # customize kernel version
+
+  users.groups.admin = {};
+  users.users = {
+    admin = {
+      isNormalUser = true;
+      extraGroups = [ "wheel" ];
+      password = "yolo";
+      group = "admin";
+    };
+  };
+
+  virtualisation.vmVariant = {
+    # following configuration is added only when building VM with build-vm
+    virtualisation = {
+      memorySize = 2048; # Use 2048MiB memory.
+      cores = 3;
+      graphics = false;
+    };
+  };
+
+  services.openssh = {
+    enable = true;
+    settings.PasswordAuthentication = true;
+  };
+
+  networking.firewall.allowedTCPPorts = [ 22 ];
+  environment.systemPackages = with pkgs; [
+    htop
+  ];
+
+  system.stateVersion = "24.11";
+}
+
