84acf128c10321bcccbe483aa41324c4aa4aed24
tv <tv@krebsco.de>
2025-04-01 15:26:38 +0200

    selinux-vm: add examplesecret policy

--- configuration.nix
+++ configuration.nix
@@ -30,6 +30,9 @@
   networking.firewall.allowedTCPPorts = [ 22 ];
 
   security.selinux.enable = true;
+  security.selinux.packages = [
+    pkgs.dotnix-selinux-policy
+  ];
 
   environment.systemPackages = [
     pkgs.htop
@@ -43,7 +46,50 @@
 
   nixpkgs.overlays = [
     inputs.self.overlays.default
+    (self: super: {
+      dotnix-selinux-policy =
+        pkgs.symlinkJoin {
+          name = "dotnix-selinux-policy";
+          paths = [
+            (pkgs.selinux.makeModule "dotnix/examplesecret" ''
+              module examplesecret 1.0;
+
+              require {
+                class dir { getattr open read search };
+                class file { getattr open read relabelto };
+                class filesystem { associate };
+                type fs_t;
+                type unconfined_t;
+              }
+
+              type examplesecret_t;
+
+              allow examplesecret_t fs_t:filesystem associate;
+              allow unconfined_t examplesecret_t:dir { getattr open read search };
+              allow unconfined_t examplesecret_t:file { getattr open read relabelto };
+            '')
+          ];
+        };
+    })
   ];
+
+  systemd.services.dotnix-selinux-setup = {
+    description = "Dotnix SELinux Setup";
+    requiredBy = [ "multi-user.target" ];
+    after = [ "selinux-modular-setup.service" ];
+    serviceConfig = {
+      ExecStart = pkgs.writers.writeDash "dotnix-selinux-setup" ''
+        if ! test -e /examplesecret; then
+          mkdir -m 0777 /examplesecret
+          echo this is an examplesecret > /examplesecret/examplesecret.txt
+          ${pkgs.selinux.coreutils}/bin/chcon -t examplesecret_t /examplesecret
+          ${pkgs.selinux.coreutils}/bin/chcon -t examplesecret_t /examplesecret/examplesecret.txt
+        fi
+      '';
+      Type = "oneshot";
+      RemainAfterExit = true;
+    };
+  };
 
   system.stateVersion = "24.11";
 }
