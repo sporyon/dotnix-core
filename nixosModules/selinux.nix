b5266774cdd53e9c19bd519a7463a9abd9357d7e
tv <tv@krebsco.de>
2025-04-09 23:53:06 +0200

    selinux: use structured kernel config

--- nixosModules/selinux.nix
+++ nixosModules/selinux.nix
@@ -48,13 +48,9 @@
       {
         name = "selinux-config";
         patch = null;
-        extraConfig = ''
-          DEFAULT_SECURITY_SELINUX n
-          SECURITY_SELINUX y
-          SECURITY_SELINUX_AVC_STATS y
-          SECURITY_SELINUX_BOOTPARAM n
-          SECURITY_SELINUX_DEVELOP y
-        '';
+        extraStructuredConfig = {
+          SECURITY_SELINUX = lib.kernel.yes;
+        };
       }
     ];
 
