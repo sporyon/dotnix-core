f05d4707969efeeb2f1b91cf69fe8f693e428024
tv <tv@krebsco.de>
2025-04-09 22:58:16 +0200

    selinux: build policy declaratively

--- nixosModules/selinux.nix
+++ nixosModules/selinux.nix
@@ -2,20 +2,45 @@
   options = {
     security.selinux.enable = lib.mkEnableOption "SELinux";
 
+    security.selinux.policy = lib.mkOption {
+      description = ''
+        This is the SELinux policy to be installed.
+
+        When installing a custom policy, consider whether any parts of
+        `security.selinux.packages` should be incorporated, as these
+        packages are included in the default policy.
+      '';
+      default = pkgs.selinux.makePolicy "selinux-policy" {
+        base = pkgs.selinux.makePolicy "selinux-policy-base" {
+          packages = [ pkgs.selinux.refpolicy ];
+        };
+        packages = config.security.selinux.packages;
+        mode = "permissive";
+        store = "strict";
+      };
+      defaultText = lib.literalExpression ''
+        pkgs.selinux.makePolicy "selinux-policy" {
+          base = pkgs.selinux.makePolicy "selinux-policy-base" {
+            packages = [ pkgs.selinux.refpolicy ];
+          };
+          packages = config.security.selinux.packages;
+          mode = "permissive";
+          store = "strict";
+        }
+      '';
+    };
+
     security.selinux.packages = lib.mkOption {
       description = ''
-        Packages containing SELinux policy that should be installed.
+        These are packages containing SELinux modules that should be built into
+        the default `security.selinux.policy`.
+
+        This option provides a convenient way to add custom policy modules
+        based on `pkgs.selinux.refpolicy`. To install a policy with a different
+        base or without any base, use `security.selinux.policy` instead.
       '';
       type = lib.types.listOf lib.types.package;
       default = [];
-    };
-    security.selinux.refpolicy = lib.mkOption {
-      description = ''
-        SELinux reference policy that should be installed.
-      '';
-      type = lib.types.nullOr lib.types.package;
-      default = pkgs.selinux.refpolicy;
-      defaultText = lib.literalExpression "pkgs.selinux.refpolicy";
     };
   };
   config = lib.mkIf config.security.selinux.enable {
@@ -37,91 +62,30 @@
       "security=selinux"
     ];
 
-    environment.etc."selinux/config".text = ''
-      SELINUX=permissive
-      SELINUXTYPE=strict
+    environment.etc =
+      lib.genAttrs [
+        "selinux/config"
+        "selinux/semanage.conf"
+        "selinux/${config.security.selinux.policy.meta.store}/policy/policy.${toString config.security.selinux.policy.meta.policyVersion}"
+        "selinux/${config.security.selinux.policy.meta.store}/seusers"
+        "selinux/${config.security.selinux.policy.meta.store}/contexts/files/file_contexts"
+        "selinux/${config.security.selinux.policy.meta.store}/contexts/files/file_contexts.bin"
+        "selinux/${config.security.selinux.policy.meta.store}/contexts/files/file_contexts.homedirs"
+        "selinux/${config.security.selinux.policy.meta.store}/contexts/files/file_contexts.homedirs.bin"
+      ] (name: {
+        source = "${config.security.selinux.policy}/etc/${name}";
+      });
+
+    system.activationScripts.selinux = ''
+      mkdir -p /var/lib/selinux
+      ${pkgs.rsync}/bin/rsync \
+          --chmod=D700,F600 \
+          --delete \
+          --recursive \
+          ${config.security.selinux.policy}/lib/selinux/${config.security.selinux.policy.meta.store} \
+          /var/lib/selinux
     '';
-
-    environment.etc."selinux/semanage.conf".text = ''
-      compiler-directory = ${pkgs.policycoreutils}/libexec/selinux/hll
-
-      [load_policy]
-      path = ${pkgs.policycoreutils}/bin/load_policy
-      [end]
-
-      [sefcontext_compile]
-      path = ${pkgs.libselinux}/bin/sefcontext_compile
-      args = $@
-      [end]
-
-      [setfiles]
-      path = ${pkgs.policycoreutils}/bin/setfiles
-      args = -q -c $@ $<
-      [end]
-    '';
-
-    environment.etc."selinux/packages".text = ''
-      ${lib.concatMapStringsSep "\n"
-          (package: "${package.pname or package.name} ${package}")
-          config.security.selinux.packages
-      }
-    '';
-
-    security.selinux.packages =
-      lib.mkBefore
-        (lib.optional
-          (config.security.selinux.refpolicy != null)
-          config.security.selinux.refpolicy);
 
     systemd.package = pkgs.selinux.systemd;
-
-    systemd.services.selinux-modular-setup = {
-      description = "Modular SELinux Setup";
-      requiredBy = [
-        "sysinit.target"
-      ];
-      requires = [
-        "system.slice"
-      ];
-      before = [
-        "sysinit.target"
-      ];
-      after = [
-        "local-fs.target"
-        "system.slice"
-      ];
-      unitConfig = {
-        DefaultDependencies = false;
-      };
-      restartTriggers = [
-        config.environment.etc."selinux/packages".text
-      ];
-      serviceConfig = {
-        ExecStart = pkgs.writers.writeDash "selinux-modular-setup" ''
-          set -efu
-
-          # Ensure module store exists
-          mkdir -p /var/lib/selinux/strict
-
-          # Install policies
-          cat /etc/selinux/packages |
-          while read -r name package; do
-            marker=/var/lib/selinux/strict/$name
-            if test "$package" != "$(readlink "$marker")";  then
-              echo "install $name $package" >&2
-              find "$package/share/selinux" -name \*.pp \
-                  -exec ${pkgs.policycoreutils}/bin/semodule --noreload --install {} +
-              ln -fns "$package" "$marker"
-            fi
-          done
-
-          # Load installed policy
-          ${pkgs.policycoreutils}/bin/semodule --reload
-        '';
-        Type = "oneshot";
-        RemainAfterExit = true;
-      };
-    };
   };
 }
-
