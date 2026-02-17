c97f122747c73519788bcd818a03436b40e5c251
root <root@dotnix>
2026-02-17 00:26:13 +0000

    example: enable rebuild within VM

--- example.nix
+++ example.nix
@@ -13,6 +13,10 @@
 
       boot.kernelParams = [ "hidepid=2" ];
 
+      # Enable rebuild within a VM
+      # usage: nixos-rebuild switch --flake /etc/nixos#example-$(uname -m)-linux
+      environment.etc.nixos.source = inputs.self;
+
       # On a real system root would either not get a password at all or it
       # would be configured using hashedPassword.
       # For the Docker image this is good enough :)
