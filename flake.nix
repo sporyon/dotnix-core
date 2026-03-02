4a5199031e1d0afa6990cddd65fbd0d232c2a819
root <root@dotnix>
2026-03-02 16:14:44 +0000

    nixpkgs: 8bb5646 -> 1267bb4 (25.11)

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
     flake-parts.url = "github:hercules-ci/flake-parts";
     lanzaboote.url = "github:nix-community/lanzaboote/v1.0.0";
     lanzaboote.inputs.nixpkgs.follows = "nixpkgs";
-    nixpkgs.url = "github:NixOS/nixpkgs/8bb5646e0bed5dbd3ab08c7a7cc15b75ab4e1d0f"; # nixos-25.11 @ 2025-11-30T17:11:29Z
+    nixpkgs.url = "github:NixOS/nixpkgs/1267bb4920d0fc06ea916734c11b0bf004bbe17e"; # nixos-25.11 @ 2026-02-25T19:16:40Z
     polkadot.url = "github:andresilva/polkadot.nix/a2eac4baedef48acb30eb869a7f859265d89c915";
     polkadot.inputs.nixpkgs.follows = "nixpkgs";
   };
